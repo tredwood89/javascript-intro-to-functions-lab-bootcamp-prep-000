bd62282520158cdf99fb1806d35c0d54d2e12603
tredwood89 <tredwood89@gmail.com>
2017-09-23 03:11:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(STRING) {
+  return STRING.toLowerCase()
+}
+
+
+function logShout(string) { console.log (string.toUpperCase() )
+}
+
+function logWhisper(STRING) {console.log (STRING.toLowerCase() )
+}
+
 
 
   function sayHiToGrandma(string){
