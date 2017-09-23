c06d629a9d8c45d698b4de265b58e9ee97d8cf9b
tredwood89 <tredwood89@gmail.com>
2017-09-23 03:11:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-function shout(string){
-  return string.toUpperCase()
-}
-
-function whisper(STRING) {
-  return STRING.toLowerCase()
-}
-
-
-function logShout(string) { console.log (string.toUpperCase() )
-}
-
-function logWhisper(STRING) {console.log (STRING.toLowerCase() )
-}
-
 
 
 
