0ccc6d22d99c9f1038a7ed8e1ab11ec92d3f8c45
tredwood89 <tredwood89@gmail.com>
2017-09-23 03:07:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,15 +24,14 @@ function sayHiToGrandma(  I love you, Grandma.) {
 
 
 
-  function sayHiToGrandma(string){		  
-   var uppercase = "HELLO"		
-   var lowercase = "hello"		
- 
-  if (string === lowercase){		 
- return "I can't hear you!"		      
+  function sayHiToGrandma(string){
+   var uppercase = "HELLO"
+   var lowercase = "hello"
+
+  if (string === lowercase){
+ return "I can't hear you!"
   }else if (string === uppercase){		 
- return "YES INDEED!"		      
-}else if (string === "I love you, Grandma."){		    
- return "I love you, too."		      
+ return "YES INDEED!"
+}else if (string === "I love you, Grandma."){
+ return "I love you, too."
  }
-  
