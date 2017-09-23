da643e234a0be197d4b4f8a0d4d9297d58bc1c0a
tredwood89 <tredwood89@gmail.com>
2017-09-23 03:08:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function logWhisper(STRING) {console.log (STRING.toLowerCase() )
 
   if (string === lowercase){
  return "I can't hear you!"
-  }else if (string === uppercase){		 
+  }else if (string === uppercase){
  return "YES INDEED!"
 }else if (string === "I love you, Grandma."){
  return "I love you, too."
