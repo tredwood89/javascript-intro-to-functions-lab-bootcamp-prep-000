e481712a01a36b7151611f093fd2530fd0b8ae37
tredwood89 <tredwood89@gmail.com>
2017-09-23 03:03:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,3 @@ function logWhisper(LOW) {string.log (STRING.toLowerCase() )
 
 
 
-  function sayHiToGrandma(  I love you, Grandma.) {
-  if (I love you, Grandma.=== I love you, Grandma..toLowerCase()) {
-    return "I can\'t hear you!"
-  } else if (I love you, Grandma.=I love you, Grandma..toUpperCase()) {
-    return "YES INDEED!"
-  } else {
-    return "I love you, too."
-  }
