df17b6d0ec9fa5cce2cbc6af043213b084210bb3
tredwood89 <tredwood89@gmail.com>
2017-09-23 01:51:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,4 @@ function logShout(loud) { console.log (loud.toUpperCase() )
 function logWhisper(LOW) {console.log (LOW.toLowerCase() )
 }
 
-<<<<<<< HEAD
-function sayHiToGrandma(HOME) {return "YES INDEED" }
-=======
-function sayHiToGrandma(HOME) {return "YES INDEED" }
->>>>>>> da4eb9d42729c73c9a6920a0e0446f640a82249e
+
