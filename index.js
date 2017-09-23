ef6eb5e25a071366cb1dca85d496b9fbdbde1978
tredwood89 <tredwood89@gmail.com>
2017-09-23 03:07:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ function logShout(string) { console.log (string.toUpperCase() )
 
 function logWhisper(STRING) {console.log (STRING.toLowerCase() )
 }
+
+function sayHiToGrandma(  I love you, Grandma.) {
+  if (I love you, Grandma.=== I love you, Grandma..toLowerCase()) {
+    return "I can\'t hear you!"
+  } else if (I love you, Grandma.=I love you, Grandma..toUpperCase()) {
+    return "YES INDEED!"
+  } else {
+    return "I love you, too."
+  }
+
+
+
+  function sayHiToGrandma(string){		  
+   var uppercase = "HELLO"		
+   var lowercase = "hello"		
+ 
+  if (string === lowercase){		 
+ return "I can't hear you!"		      
+  }else if (string === uppercase){		 
+ return "YES INDEED!"		      
+}else if (string === "I love you, Grandma."){		    
+ return "I love you, too."		      
+ }
+  
